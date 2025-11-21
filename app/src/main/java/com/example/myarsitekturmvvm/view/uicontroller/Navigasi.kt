7b33a4bf74acf9bb93a89ea323c925a5e69eb14f
nachool <nachma.olivia1394@gmail.com>
2025-11-21 16:19:57 +0700

    error handling

--- app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/uicontroller/Navigasi.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myarsitekturmvvm.view.FormSiswa
+import com.example.myarsitekturmvvm.view.TampilSiswa
+import com.example.myarsitekturmvvm.viewmodel.SiswaViewModel
 
 enum class Navigasi {
     Formulirku,
@@ -15,26 +20,32 @@ enum class Navigasi {
 }
 @Composable
 fun DataApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel (),
     navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
 
             modifier = Modifier.padding(paddingValues = isiRuang)) {
             composable(route = Navigasi.Formulirku.name){
-                FormIsian (
-                    OnSubmitBtnClick = {
+                val konteks = LocalContext.current
+                FormSiswa (
+                    pilihanJK = JenisK.map {id -> konteks.resources.getString(id)},
+                    OnSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
                         navController.navigate(route = Navigasi.Detail.name)
                     }
+
                 )
             }
             composable(route = Navigasi.Detail.name){
-                TampilData(
-                    onBackBtnClick = {
-                        cancelAndBackToFormulir(navController)
-                    }
+                TampilSiswa(
+                    statusUISiswa = uiState.value,
+                    OnSubmitButtonClicked = { cancelAndBackToFormulir(navController)}
                 )
             }
         }
